0d5fe2f64edcbb7fe065dc91a651999d9d2ccb6e
triavago <43296373+triavago@users.noreply.github.com>
2023-05-18 11:38:44 +0000

    add simulate pond behave

--- test.cpp
+++ test.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<random>
 #include <memory>
+#include<algorithm>
 
 using namespace std;
 
@@ -37,6 +38,12 @@ public:
             alive = false;
         }
     }
+
+    void weaken()
+    {
+        size -= 1;
+        cout << "Fish size: " << size <<endl;
+    }
 private:
     int size;
     bool alive;
@@ -66,9 +73,80 @@ public:
     Pond(int fishNums)
     {
         fishes.reserve(fishNums);
-            
+        createFish(fishNums);    
+    }
+
+    void simulatePond()
+    {
+        while(fishes.size() >= 1)
+        {
+            for(auto& currentFish : fishes)
+            {
+                for(auto& otherFish : fishes)
+                {
+                    if(currentFish->getSize() > otherFish->getSize())
+                    {
+                        currentFish->eat(*otherFish);
+                    }
+                    else
+                    {
+                        otherFish->eat(*currentFish);
+                        break;
+                    }
+                }
+                currentFish->weaken();
+            }
+            printFishState();
+            cout << "end loop" << endl;
+            removeDeadFish();
+        }
+    }
+
+    void printFishState()
+    {
+        int i = 0;
+        for(const auto& currentFish : fishes)
+        {
+            i++;
+            if(currentFish->isAlive())
+                cout << "Fish " << i << ": size " << currentFish->getSize() << endl;
+            else
+                cout << "Fish " << i << " is dead" << endl;
+        }
     }
 private:
     vector<unique_ptr<Fish>> fishes;
 
+    void createFish(int numFish)
+    {
+        random_device rd;
+        mt19937 gen(rd());
+        uniform_int_distribution<> dis(1, 10);
+
+        for (int i = 0; i < numFish; i++) 
+        {
+            int size = dis(gen);
+            fishes.push_back(make_unique<Fish>(size));
+        }
+    }
+
+    void removeDeadFish()
+    {
+        fishes.erase(remove_if(fishes.begin(), fishes.end(), [](const unique_ptr<Fish>& f) {
+            return !f->isAlive();
+        }), fishes.end());
+    }
 };
+
+int main()
+{
+    Pond pond(5);
+
+    for(int i = 0; i < 5; i++)
+    {
+        pond.simulatePond();
+        pond.printFishState();
+    }
+
+    return 0;
+}
